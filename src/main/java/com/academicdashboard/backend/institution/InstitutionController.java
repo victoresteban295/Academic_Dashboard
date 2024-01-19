b79f681359ae536f29e2bc1e87ad46da10a48366
victoresteban295 <esteban.victor295@gmail.com>
2024-01-19 15:57:46 -0800

    Refactor Endpoints for DepartmentController & InstitutionController

--- src/main/java/com/academicdashboard/backend/institution/InstitutionController.java
+++ src/main/java/com/academicdashboard/backend/institution/InstitutionController.java
@@ -6,22 +6,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequestMapping("/api/auth/institution")
+@RequestMapping("/v1.0/auth")
 @RequiredArgsConstructor
 public class InstitutionController {
 
     private final InstitutionService institutionService;
 
     //Creates New Academic Institution | Returns Institution Created
-    @PostMapping("/new")
+    @PostMapping("/institutions")
     public ResponseEntity<Institution> createInstitution(@RequestBody Map<String, String> payload) {
         return new ResponseEntity<Institution>(
                 institutionService.createInstitution(
@@ -32,31 +34,37 @@ public class InstitutionController {
 
 
     //Modify Existing Institution | Returns Modified Institution
-    @PutMapping("/modify")
-    public ResponseEntity<Institution> modifyInstitution(@RequestBody Map<String, String> payload) {
+    @PatchMapping("/institutions/{schoolId}")
+    public ResponseEntity<Institution> modifyInstitution(
+            @PathVariable String schoolId, 
+            @RequestBody Map<String, String> payload) {
+
         return new ResponseEntity<Institution>(
                 institutionService.modifyInstitution(
-                    payload.get("schoolId"),
-                    payload.get("newName")
+                    schoolId,
+                    payload.get("schoolName")
                 ),
                 HttpStatus.OK);
     }
 
     //Delete Existing Institution | Void
-    @DeleteMapping("/delete")
-    public ResponseEntity<Void> deleteInstitution(@RequestBody Map<String, String> payload) {
-        institutionService.deleteInstitution(payload.get("schoolId"));
+    @DeleteMapping("/institutions/{schoolId}")
+    public ResponseEntity<Void> deleteInstitution(@PathVariable String schoolId) {
+        institutionService.deleteInstitution(schoolId);
         return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     } 
 
-    @PostMapping("/verify")
-    public ResponseEntity<VerificationResponse> verifyInstitution(@RequestBody Map<String, String> payload) {
+    //Get Institution Informaiton Based on Profile || Returns VerificatonResponse
+    @GetMapping("/institutions/profiles/{profile}/{codeId}")
+    public ResponseEntity<VerificationResponse> verifyInstitution(
+            @PathVariable String profile, 
+            @PathVariable String codeId) {
+
         return new ResponseEntity<VerificationResponse>(
                 institutionService.verifyInstitution(
-                    payload.get("profile"), 
-                    payload.get("codeId")
+                    profile, 
+                    codeId
                 ),
                 HttpStatus.OK);
     }
-
 }
